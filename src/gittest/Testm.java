47ed4e7594ab134587249e91ba2e26e7cc249be6
dong4j <dongshijie@fkhwl.com>
2017-06-20 14:22:58 +0800

    test idea submit

--- src/gittest/Testm.java
+++ src/gittest/Testm.java
@@ -3,7 +3,7 @@ package gittest;
 public class Testm {
 	public static void main(String[] args) {
 		
-		System.out.println("f111ix issue2 aaa");
+		System.out.println("f111ix issue2 aaa 222");
 		System.out.println("I am master!!! you know, continue fix");
 	}
 }
