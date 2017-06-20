5491939314cbe20e2e6f47dc9b3eb04cccacc453
dong4j <dongshijie@fkhwl.com>
2017-06-20 09:56:50 +0800

    test

--- src/gittest/Testm.java
+++ src/gittest/Testm.java
@@ -3,6 +3,6 @@ package gittest;
 public class Testm {
 	public static void main(String[] args) {
 		
-		System.out.println("I am master!!! you know");
+		System.out.println("I am dev!!! you know1");
 	}
 }
